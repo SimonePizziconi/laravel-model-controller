e7064fcd1b3546e6d621ffe7eec278dc02cad2a8
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-11 16:41:52 +0200

    feat: create query for movies and foreach loop for print the card in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
 {{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
 @extends('layouts.main')
 
-@section('hero')
-    <div>HERO HOME</div>
-@endsection
-
 @section('content')
     <div class="container my-5">
         <h1>Home</h1>
-        <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente blanditiis dolor itaque possimus nisi quia a,
-            natus culpa, sint maxime minus, voluptatum quos perspiciatis quas! Autem recusandae eius dolorum dicta?
-        </p>
-
+        <div class="container my-5 d-flex flex-wrap gap-3">
+            @foreach ($movies as $movie)
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Lingua originale: {{ $movie->nationality }}</h6>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Data di uscita: {{ $movie->date }}</h6>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Voto: {{ $movie->vote }}</h6>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
 
